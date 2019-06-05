cb3625fcc664b084e6bbeb36a0f25cadcb236ff7
SofiaFredriksson <sofiapkf@gmail.com>
2019-06-05 13:40:41 +0100

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -1,17 +1,15 @@
 class Project
-
   attr_reader :title
   attr_accessor :backers
 
   def initialize(title)
-    @title = title
+    @title = title 
     @backers = []
   end
 
   def add_backer(backer)
-    @backers << backer
-    if !backer.backed_projects.include?(self)
-      backer.back_project(self)
-    end
+    backers << backer
+    backer.backed_projects << self
   end
+
 end
